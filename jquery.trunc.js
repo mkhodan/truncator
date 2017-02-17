0d8bfdc2c838b65ca871f047b6b28f2b9d202259
Mykola Khodan <mykola.khodan@gmail.com>
2017-02-17 13:36:45 +0200

    Add files via upload

---
+++ jquery.trunc.js
@@ -0,0 +1,25 @@
+$.fn.trunc = function(size = 100) {
+    if(typeof(size) === 'number'){
+	    this.each((i, el) => {
+	        var value = el.innerHTML;
+	        if (value.length > Math.abs(size) && size !== 0) {
+	            if (size > 0) {
+	                var valueTruncated = value.substring(0, size);
+	            } else {
+	                var valueTruncated = value.substring(0, value.length + size);
+	            }
+	            var span = document.createElement('span');
+	            span.innerHTML = '&hellip;';
+	            span.style.cursor = 'pointer';
+	            el.innerHTML = valueTruncated;
+	            span.addEventListener('click', () => {
+	                el.innerHTML = value;
+	            });
+	            el.appendChild(span);
+	        }
+	    });
+	}
+	    return this;
+}
+
+$('.trnc').trunc(223);
